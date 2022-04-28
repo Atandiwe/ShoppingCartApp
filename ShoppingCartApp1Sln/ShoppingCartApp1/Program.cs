b1a108d1a4ccb099520cc5200305d071548c0a91
Atandiwe <102726464+Atandiwe@users.noreply.github.com>
2022-04-28 10:36:13 +0200

    added new class

---
+++ ShoppingCartApp1Sln/ShoppingCartApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingLibrary;
+using System;
+
+namespace ShoppingCartApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCart app = new ShoppingCart();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
